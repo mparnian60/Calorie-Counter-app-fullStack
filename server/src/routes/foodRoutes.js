80b7e1807998e39df82f8163437a1d836f3e14ea
Mona Parnian <m.parnian.eng@gmail.com>
2020-08-15 09:17:06 +1000

    Update route for create new record- don't push any meal without servingsize into array

--- server/src/routes/foodRoutes.js
+++ server/src/routes/foodRoutes.js
@@ -41,52 +41,54 @@ router.use((req, res, next) => {
   })
 })
 
-
-// create new plan
-router.post('/newDayPlan', async (req, res) => {
-
-  //convert date format comes from body to mongoDB format
-  const firstFormat = moment.utc(req.body.date); // formats to ISO 8601 String
+//convert date format comes from body to mongoDB format
+const changeDateFormat = (date)=>{
+  const firstFormat = moment.utc(date); // formats to ISO 8601 String
   const toUnix = moment(firstFormat).format()
   removeZ = toUnix.substring(0, 19)
   otherVar = ".000+00:00"
   finalValue = removeZ.concat(otherVar)
-  // console.log('final value', finalValue);
+  return finalValue;
+}
 
-  try {
 
-    //checking whether combination of userID & date exist
-    const findExistingPlan = await DayPlan.find({ userId: req.body.userId, date: finalValue })
-    // console.log('date', moment(req.body.date).toISOString());
-    if (findExistingPlan.length < 1) {
-      const newDayPlan = await DayPlan.create(req.body);
-      res.status(200).send(newDayPlan);
-    } else {
-      console.log("findExistingPlan", findExistingPlan);
-      // res.send("plan exist");
-      res.redirect(307, '/api/food/updateDayPlan');
-    }
+// create new plan
+// router.post('/newDayPlan', async (req, res) => {
 
-  } catch (e) {
-    console.log('create error', e);
-    res.status(400).send("Bad request");
-  }
-})
+//   const date = changeDateFormat(req.body.date);
+//   console.log('date', date);
+
+//   try {
+
+//     //checking whether combination of userID & date exist
+//     const findExistingPlan = await DayPlan.find({ userId: req.body.userId, date: date })
+//     console.log('findexisting plan', findExistingPlan)
+  
+//     if (findExistingPlan.length < 1) {
+//       const newDayPlan = await DayPlan.create(req.body);
+//       console.log('newdayplan', newDayPlan)
+
+//       res.status(200).send(newDayPlan);
+//     } else {
+//       console.log("findExistingPlan", findExistingPlan);
+//       // res.send("plan exist");
+//       res.redirect(307, '/api/food/updateDayPlan');
+//     }
+
+//   } catch (e) {
+//     console.log('create error', e);
+//     res.status(400).send("Bad request");
+//   }
+// })
 
 // update plan
 router.post('/updateDayPlan', async (req, res) => {
 
-  //convert date format comes from body to mongoDB format
-  const firstFormat = moment.utc(req.body.date); // formats to ISO 8601 String
-  const toUnix = moment(firstFormat).format()
-  removeZ = toUnix.substring(0, 19)
-  otherVar = ".000+00:00"
-  finalValue = removeZ.concat(otherVar)
-  // console.log('final value', finalValue);
+  const date = changeDateFormat(req.body.date);
 
   try {
     // find all plans under below userID
-    const findExistingPlan = await DayPlan.find({ userId: req.user.id , date: finalValue })
+    const findExistingPlan = await DayPlan.find({ userId: req.user.id , date: date })
     // console.log('req.body',req.body);
 
     //conditions to add new record to the existing one
@@ -124,19 +126,101 @@ router.post('/updateDayPlan', async (req, res) => {
 //get dayPlan
 router.post('/dayPlan', async (req, res) => {
   try {
-    //convert date format comes from body to mongoDB format
-    const firstFormat = moment.utc(req.body.date); // formats to ISO 8601 String
-    const toUnix = moment(firstFormat).format()
-    removeZ = toUnix.substring(0, 19)
-    otherVar = ".000+00:00"
-    finalValue = removeZ.concat(otherVar)
+    
+    const date = changeDateFormat(req.body.date);
 
-    const dayPlan = await DayPlan.find({ userId: req.user.id, date: finalValue });
+    const dayPlan = await DayPlan.find({ userId: req.user.id, date: date });
       res.status(200).send(dayPlan);
   }catch(e){
     console.log("e",e);
     res.status(400).send("bad request");
 }
+})
+
+//delete a food from a meal
+router.post('/updateDayPlan', async (req, res) => {
+
+  const date = changeDateFormat(req.body.date);
+
+  try {
+    // find all plans under below userID
+    const findExistingPlan = await DayPlan.find({ userId: req.user.id , date: date })
+    // console.log('req.body',req.body);
+
+    //conditions to add new record to the existing one
+    if (findExistingPlan.length >= 1) {
+      // console.log('findExistingPlan', findExistingPlan[0]);
+      const existingPlan = findExistingPlan[0];
+      // console.log('servingsize', req.body.meal.breakfast[0].servingSize);
+      if(req.body.meal.breakfast[0].servingSize){
+        existingPlan.meal.breakfast.push(req.body.meal.breakfast[0]);
+      }
+      if(req.body.meal.lunch[0].servingSize){
+        existingPlan.meal.lunch.push(req.body.meal.lunch[0]);
+      }
+      if(req.body.meal.dinner[0].servingSize){
+        existingPlan.meal.dinner.push(req.body.meal.dinner[0]);
+      }
+      if(req.body.meal.snack[0].servingSize){
+        existingPlan.meal.snack.push(req.body.meal.snack[0]);
+      }
+
+      // console.log('existingPlan',existingPlan);
+      // find a plans with chosen date and updat that one
+      const updateExistingPlan = await DayPlan.findOneAndUpdate({ date: finalValue }, {$set:existingPlan}, { new: true })
+      res.status(200).send(updateExistingPlan);
+    } else {
+      res.redirect(307, '/api/food/newDayPlan');
+    }
+  } catch (e) {
+    // console.log(e);
+    res.status(400).send("Bad request");
+  }
+})
+
+// create new plan
+router.post('/newDayPlan', async (req, res) => {
+
+  const date = changeDateFormat(req.body.date);
+
+  try {
+
+    //checking whether combination of userID & date exist
+    const findExistingPlan = await DayPlan.find({ userId: req.body.userId, date: date })
+  
+    if (findExistingPlan.length < 1) {
+
+      const newDayPlan = await DayPlan.create(req.body);
+      console.log('newday plan', newDayPlan);
+
+      const updateCreatedPlan = newDayPlan;
+
+      if(req.body.meal.breakfast[0].servingSize === 0){
+        updateCreatedPlan.meal.breakfast.pop(req.body.meal.breakfast[0]);
+      }
+      if(req.body.meal.lunch[0].servingSize === 0){
+        updateCreatedPlan.meal.lunch.pop(req.body.meal.lunch[0]);
+      }
+      if(req.body.meal.dinner[0].servingSize === 0){
+        updateCreatedPlan.meal.dinner.pop(req.body.meal.dinner[0]);
+      }
+      if(req.body.meal.snack[0].servingSize === 0){
+        updateCreatedPlan.meal.snack.pop(req.body.meal.snack[0]);
+      }
+
+      const updateExistingPlan = await DayPlan.findOneAndUpdate({ date: date }, {$set:updateCreatedPlan}, { new: true })
+
+      res.status(200).send(updateExistingPlan);
+    } else {
+      console.log("findExistingPlan", findExistingPlan);
+      // res.send("plan exist");
+      res.redirect(307, '/api/food/updateDayPlan');
+    }
+
+  } catch (e) {
+    console.log('create error', e);
+    res.status(400).send("Bad request");
+  }
 })
    
 
