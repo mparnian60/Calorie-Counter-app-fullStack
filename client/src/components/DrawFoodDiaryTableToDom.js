1f5cdde05b97de38d1a3d9bab963f06b8af03587
Mona Parnian <m.parnian.eng@gmail.com>
2020-09-22 16:40:27 +1000

    modification of dayplanAPI

--- client/src/components/DrawFoodDiaryTableToDom.js
+++ client/src/components/DrawFoodDiaryTableToDom.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {deletfoodAPI} from '../api/dayPlanAPI'
+import {deletfoodAPI} from '../api/DayPlanAPI'
 
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
